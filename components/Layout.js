910971a7e6ef46dab363d309a5a5d9abccfcd35e
disizali <a.hassssani@gmail.com>
2019-12-11 10:18:59 +0330

    remove react-ga

--- components/Layout.js
+++ components/Layout.js
@@ -3,18 +3,18 @@ import Navbar from "./Navbar";
 import Head from "next/head";
 import "../styles/index.scss";
 import Footer from "./Footer";
-import { initGA, logPageView } from '../utils/analytics'
+// import { initGA, logPageView } from '../utils/analytics'
 export default class Layout extends React.Component {
   constructor(props) {
     super(props);
   }
-  componentDidMount () {
-    if (!window.GA_INITIALIZED) {
-      initGA()
-      window.GA_INITIALIZED = true
-    }
-    logPageView()
-  }
+  // componentDidMount () {
+  //   if (!window.GA_INITIALIZED) {
+  //     initGA()
+  //     window.GA_INITIALIZED = true
+  //   }
+  //   logPageView()
+  // }
   
   render() {
     return (
