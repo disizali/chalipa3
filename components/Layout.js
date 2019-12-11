60492834b96a3680d5da4bd77a333f4d0a197bb8
disizali <a.hassssani@gmail.com>
2019-12-11 10:41:26 +0330

    remove all ga

--- components/Layout.js
+++ components/Layout.js
@@ -9,24 +9,24 @@ export default class Layout extends React.Component {
     super(props);
     this.gtag = this.gtag.bind(this);
   }
-  gtag() {
-    window.dataLayer = window.dataLayer || [];
-    dataLayer.push(arguments);
-  }
-  componentDidMount() {
-    this.gtag("js", new Date());
-    this.gtag("config", "UA-154429283-1");
-  }
+  // gtag() {
+  //   window.dataLayer = window.dataLayer || [];
+  //   dataLayer.push(arguments);
+  // }
+  // componentDidMount() {
+  //   this.gtag("js", new Date());
+  //   this.gtag("config", "UA-154429283-1");
+  // }
 
   render() {
     return (
       <main>
-        <Head>
+        {/* <Head>
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-154429283-1"
           ></script>
-        </Head>
+        </Head> */}
         <Navbar />
         <div className="children">{this.props.children}</div>
         <Footer articles={this.props.articles} />
